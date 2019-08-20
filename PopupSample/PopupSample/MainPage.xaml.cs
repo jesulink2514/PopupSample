e3bee76d9d688b764b9f61d6440e2a15302dcee6
jesulink2514 <jesus.angulo@outlook.com>
2019-08-19 20:47:07 -0500

    Add project files.

---
+++ PopupSample/PopupSample/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PopupSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            LstTest.ItemsSource = new string[]
+            {
+                "Test 1",
+                "Test 2",
+                "Test 3",
+                "Test 4",
+                "Test 5",
+                "Test 6"
+            };
+
+        }
+
+        private async void LstTest_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var page = new RegisterPopup();
+            await PopupNavigation.Instance.PushAsync(page);
+        }
+    }
+}
